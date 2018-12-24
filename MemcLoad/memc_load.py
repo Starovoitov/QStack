31ede3c7692c176e51f32ff8d2c428bda5fb0373
Starovoitov <arteom.starovoitov@yandex.ru>
2018-12-24 04:46:54 +0300

    multiprocessing is returned back - python is slow so there is a sense to use it. So, file is divided by chunks, one chunk per process. Think it's pretty optimal by speed but still need to get rid of hardcoded number of threads per process on line 128

--- MemcLoad/memc_load.py
+++ MemcLoad/memc_load.py
@@ -19,7 +19,7 @@ from functools import partial
 import multiprocessing
 from time import time, sleep
 
-lines = 0
+
 NORMAL_ERR_RATE = 0.01
 AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
 
@@ -31,6 +31,7 @@ def dot_rename(path):
 
 
 def insert_appsinstalled(memc_pool, memc_addr, appsinstalled, dry_run=False):
+    """Writes installed applications to memcache"""
     ua = appsinstalled_pb2.UserApps()
     ua.lat = appsinstalled.lat
     ua.lon = appsinstalled.lon
@@ -42,7 +43,7 @@ def insert_appsinstalled(memc_pool, memc_addr, appsinstalled, dry_run=False):
             logging.debug("%s - %s -> %s" % (memc_addr, key, str(ua).replace("\n", " ")))
         else:
             try:
-                memc = memc_pool.get(timeout=10.1)
+                memc = memc_pool.get(timeout=0.1)
             except Queue.Empty:
                 memc = memcache.Client([memc_addr], socket_timeout=3.0)
             ok = False
@@ -59,6 +60,7 @@ def insert_appsinstalled(memc_pool, memc_addr, appsinstalled, dry_run=False):
 
 
 def parse_appsinstalled(line):
+    """Returns AppsInstalled object from parsed line"""
     line_parts = line.strip().split("\t")
     if len(line_parts) < 5:
         return
@@ -79,22 +81,43 @@ def parse_appsinstalled(line):
 
 def main(options):
     ts = time()
-    fns = sorted(fn for fn in glob.iglob(options.pattern))
-    handler = partial(handle_log, options=options)
-    # print handler.keywords
-    for fn in fns:
-        handle_log(fn, options)
+    num_processes = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=num_processes)
+    fnames = sorted(fn for fn in glob.iglob(options.pattern))
+    for fn in fnames:
+        faddrs = list(getchunks(fn, num_processes))
+        handler = partial(handle_log, fn=fn, options=options)
+        for faddr in pool.imap(handler, faddrs):
+            pass
+        dot_rename(fn)
+
     logging.info('Took %s', time() - ts)
 
 
-def handle_log(fn, options):
-    global lines
+def getchunks(file_, chunks_number):
+    """Break gz file into chunks and returns list of pairs of chunk start address and offset"""
+    f = gzip.open(file_)
+    size = os.stat(file_).st_size/chunks_number
+    while 1:
+        start = f.tell()
+        f.seek(size, 1)
+        s = f.readline()
+        yield start, f.tell() - start
+        if not s:
+            break
+
+
+def handle_log(faddrs, fn, options):
+    """Handles chunk of file, counts error rate"""
     device_memc = {
         "idfa": options.idfa,
         "gaid": options.gaid,
         "adid": options.adid,
         "dvid": options.dvid,
     }
+
+    chunk_end = int(faddrs[0]) + int(faddrs[1])
+    chunk_start = int(faddrs[0])
 
     pools = collections.defaultdict(Queue.Queue)
     results = []
@@ -111,37 +134,33 @@ def handle_log(fn, options):
         thread.start()
 
     logging.info('Processing %s' % fn)
-    fd = gzip.open(fn)
-    for line in fd:
-        line = line.strip()
-        if not line:
-            continue
-        appsinstalled = parse_appsinstalled(line)
-        if not appsinstalled:
-            errors += 1
-            continue
-        memc_addr = device_memc.get(appsinstalled.dev_type)
-        if not memc_addr:
-            errors += 1
-            logging.error("Unknown device type: %s" % appsinstalled.dev_type)
-            continue
+    with gzip.open(fn) as fd:
+        fd.seek(chunk_start)
+        while int(fd.tell()) < chunk_end:
+            line = fd.readline()
+            line = line.strip()
+            if not line:
+                continue
+            appsinstalled = parse_appsinstalled(line)
+            if not appsinstalled:
+                errors += 1
+                continue
+            memc_addr = device_memc.get(appsinstalled.dev_type)
+            if not memc_addr:
+                errors += 1
+                logging.error("Unknown device type: %s" % appsinstalled.dev_type)
+                continue
 
-        job_queue.put((pools[memc_addr], memc_addr, appsinstalled, options.dry))
+            job_queue.put((pools[memc_addr], memc_addr, appsinstalled, options.dry))
 
-        if not all(thread.is_alive() for thread in workers):
-            break
+            if not all(thread.is_alive() for thread in workers):
+                break
 
     for thread in workers:
         if thread.is_alive():
             thread.join()
 
-    #while not results.empty():
-     #   processed_per_worker, errors_per_worker = results.get()
-      #  processed += processed_per_worker
-       # errors += errors_per_worker
-
     processed, errors = [sum(x) for x in zip(*results)]
-    logging.info("Lines: %d" % lines)
 
     if processed:
         err_rate = float(errors) / processed
@@ -149,15 +168,14 @@ def handle_log(fn, options):
             logging.info("Acceptable error rate (%s). Successfull load" % err_rate)
         else:
             logging.error("High error rate (%s > %s). Failed load" % (err_rate, NORMAL_ERR_RATE))
-    fd.close()
 
 
 def handle_thread(job_queue, results):
-    global lines
+    """Handles thread task to put installed applications into memcache"""
     processed = errors = 0
     while True:
         try:
-            task = job_queue.get(timeout=10.1)
+            task = job_queue.get(timeout=0.1)
         except Queue.Empty:
             results.append((processed, errors))
             return
@@ -165,8 +183,6 @@ def handle_thread(job_queue, results):
         memc_pool, memc_addr, appsinstalled, dry_run = task
         ok = insert_appsinstalled(memc_pool, memc_addr, appsinstalled, dry_run)
 
-        lines += 1
-        logging.info(lines)
         if ok:
             processed += 1
         else:
